05252d889db1a7956f25210b1825b62107f864dc
dawidplesniarski <dawidplesniarski@gmail.com>
2019-10-10 21:02:29 +0200

    GUI

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,37 @@
 package com.company;
 
+//import sun.misc.FormattedFloatingDecimal;
+
+import javax.swing.*;
+import javax.swing.table.TableColumn;
+import java.sql.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+
+
+
+
+        Connect connect = new Connect();
+        Statement statement = connect.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery("SELECT * FROM dziekanat.studenci");
+
+        JFrame frame = new JFrame();
+        GUI form = new GUI();
+
+
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setContentPane(form.getPanel());
+
+        frame.pack();
+
+        frame.setVisible(true);
+
+
+
 
     }
 }
